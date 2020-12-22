099c73e4dad60eb8351db6feca7729248ac83532
Jesse <moore.jesse@gmail.com>
2020-12-22 07:04:54 -0600

    part 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,35 @@ export class AppComponent {
   sourceList: Satellite[];
 
   constructor() {
-    this.sourceList = [
-      new Satellite('SiriusXM', 'Communication', '2009-03-21', 'LOW', true),
-      new Satellite('Cat Scanner', 'Imaging', '2012-01-05', 'LOW', true),
-      new Satellite('Weber Grill', 'Space Debris', '1996-03-25', 'HIGH', false),
-      new Satellite('GPS 938', 'Positioning', '2001-11-01', 'HIGH', true),
-      new Satellite('ISS', 'Space Station', '1998-11-20', 'LOW', true),
-    ];
+    this.sourceList = [];
+    let satellitesUrl =
+      'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesUrl).then(
+      function (response) {
+        response.json().then(
+          function (data) {
+            let fetchedSatellites = data.satellites;
+            const newSatellites = fetchedSatellites.map((satellite) => {
+              const {
+                name,
+                type,
+                launchDate,
+                orbitType,
+                operational,
+              } = satellite;
+              return new Satellite(
+                name,
+                type,
+                launchDate,
+                orbitType,
+                operational
+              );
+            });
+            this.sourceList = [...this.sourceList, ...newSatellites];
+          }.bind(this)
+        );
+      }.bind(this)
+    );
   }
 }
