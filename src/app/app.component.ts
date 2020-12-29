acf1cad063e6a5a67fdd25253525247770ef80d7
Jesse <moore.jesse@gmail.com>
2020-12-29 06:15:25 -0600

    Requirements 7 & 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
 
+interface FetchedSatellite {
+  name: string;
+  type: string;
+  launchDate: string;
+  orbitType: string;
+  operational: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +16,9 @@ import { Satellite } from './satellite';
 })
 export class AppComponent {
   title = 'orbit-report';
+  searchTerm = '';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
@@ -16,30 +26,38 @@ export class AppComponent {
       'https://handlers.education.launchcode.org/static/satellites.json';
 
     window.fetch(satellitesUrl).then(
-      function (response) {
+      function (response: any) {
         response.json().then(
-          function (data) {
+          function (data: { satellites: FetchedSatellite[] }): void {
             let fetchedSatellites = data.satellites;
-            const newSatellites = fetchedSatellites.map((satellite) => {
-              const {
-                name,
-                type,
-                launchDate,
-                orbitType,
-                operational,
-              } = satellite;
-              return new Satellite(
-                name,
-                type,
-                launchDate,
-                orbitType,
-                operational
-              );
-            });
+            const newSatellites = fetchedSatellites.map(
+              (satellite: FetchedSatellite): Satellite => {
+                const {
+                  name,
+                  type,
+                  launchDate,
+                  orbitType,
+                  operational,
+                } = satellite;
+                return new Satellite(
+                  name,
+                  type,
+                  launchDate,
+                  orbitType,
+                  operational
+                );
+              }
+            );
             this.sourceList = [...this.sourceList, ...newSatellites];
+            this.displayList = [...this.sourceList];
           }.bind(this)
         );
       }.bind(this)
     );
+  }
+  search(searchTerm: string): void {
+    this.displayList = this.sourceList.filter((sat) => {
+      return sat.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
   }
 }
--- src/app/components/orbit-list/orbit-list.component.ts
+++ src/app/components/orbit-list/orbit-list.component.ts
@@ -8,7 +8,39 @@ import { Satellite } from '../../satellite';
 })
 export class OrbitListComponent implements OnInit {
   @Input() satellites: Satellite[];
+  sortedAscending = { name: false, type: false };
   constructor() {}
 
   ngOnInit(): void {}
+
+  sort(column: string): void {
+    if (this.sortedAscending[column]) {
+      this.sortDescending(column);
+    } else {
+      this.sortAscending(column);
+    }
+    this.sortedAscending[column] = !this.sortedAscending[column];
+  }
+
+  sortAscending(column: string) {
+    this.satellites.sort(function (a: Satellite, b: Satellite): number {
+      if (a[column] < b[column]) {
+        return -1;
+      } else if (a[column] > b[column]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+  sortDescending(column: string) {
+    this.satellites.sort(function (a: Satellite, b: Satellite): number {
+      if (a[column] < b[column]) {
+        return 1;
+      } else if (a[column] > b[column]) {
+        return -1;
+      }
+      return 0;
+    });
+  }
 }
